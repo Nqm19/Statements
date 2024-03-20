c2440ecc3c4fec85780e3d33b3488bbfe01f5458
Nqm19 <164125246+Nqm19@users.noreply.github.com>
2024-03-20 12:36:42 -0500

    Create 15_alias.sql

---
+++ 15_alias.sql
@@ -0,0 +1,2 @@
+SELECT name, init_date AS 'fecha de inicio en programación' FROM users WHERE age BETWEEN 20 AND 30;
+SELECT name, init_date AS "fecha de inicio en programación" FROM users WHERE name = 'sara';
