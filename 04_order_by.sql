ff277c76abe2b09f52806ac71dccbd6e0839265a
Nqm19 <164125246+Nqm19@users.noreply.github.com>
2024-03-19 23:27:46 -0500

    Update 04_order_by.sql

--- 04_order_by.sql
+++ 04_order_by.sql
@@ -1,1 +1,4 @@
-
+SELECT * FROM users ORDER BY age;
+SELECT * FROM users ORDER BY age ASC;
+SELECT * FROM users ORDER BY age DESC;
+SELECT * FROM users WHERE email = "sara@email.com" ORDER BY age ASC;
