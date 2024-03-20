35f76ef91c525c1792f7ab88bdb38fcbde811f5e
Nqm19 <164125246+Nqm19@users.noreply.github.com>
2024-03-20 12:42:18 -0500

    Update 15_alias.sql

--- 15_alias.sql
+++ 15_alias.sql
@@ -1,2 +1,4 @@
 SELECT name, init_date AS 'fecha de inicio en programación' FROM users WHERE age BETWEEN 20 AND 30;
 SELECT name, init_date AS "fecha de inicio en programación" FROM users WHERE name = 'sara';
+SELECT CONCAT(name, surname)FROM users;
+SELECT CONCAT('Nombre: ', name, 'Apellidos: ', surname)AS 'Nombre Completo' FROM users
